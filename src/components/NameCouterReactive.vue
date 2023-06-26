4eca36382fee759dd844a76d9186d23180e2e0ee
Guangyuan <254202042@qq.com>
2023-06-26 11:01:49 +0800

    feat:使用reactive定义state

---
+++ src/components/NameCouterReactive.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    {{ state.name }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  name: 'NameCouterReactive',
+  setup() {
+    const state = reactive({
+      name: 'hello',
+    })
+    setInterval(() => {
+      state.name += '!'
+    }, 1000)
+    return {
+      state,
+    }
+  },
+})
+</script>
