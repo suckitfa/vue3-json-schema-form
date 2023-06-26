4ca113014980add2104824aa87077f3f1f3194a4
Guangyuan <254202042@qq.com>
2023-06-26 10:57:59 +0800

    feat:使用ref定义响应式的基础类型数据

---
+++ src/components/NameCouter.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    {{ name }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'NameCouter',
+  setup() {
+    const name = ref('hello')
+    setInterval(() => {
+      name.value += '!'
+    }, 1000)
+    return {
+      name,
+    }
+  },
+})
+</script>
